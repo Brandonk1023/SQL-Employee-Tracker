48d1a99efca3a21d5d9ee3c193e91a1ed0fa55e4
Brandon Kilgore <brandonk1023@gmail.com>
2023-04-13 10:39:56 -0400

    added beginning seed data for seed.sql

--- schema.sql
+++ schema.sql
@@ -3,18 +3,18 @@ CREATE DATABASE employeeInfo_db;
 USE employeeInfo_db;
 
 CREATE TABLE departments(
-    id: INT PRIMARY KEY,
+    id: INT AUTO_INCREMENT PRIMARY KEY,
     name: VARCHAR(30)
 );
 
-CREATE TABLE role(
+CREATE TABLE roles(
     id: INT PRIMARY KEY,
     title: VARCHAR(30),
     salary: DECIMAL,
     department_id: INT
 );
 
-CREATE TABLE employee(
+CREATE TABLE employees(
     id: INT PRIMARY KEY,
     first_name: VARCHAR(30),
     last_name: VARCHAR(30),
--- seeds.sql
+++ seeds.sql
@@ -5,4 +5,16 @@ VALUES
 ('Marketing'),
 ('IT')
 
-INSERT INTO 
+INSERT INTO roles (title, salary, department_id)
+VALUES
+('Accountant', 90000, 1),
+('Engineer', 100000, 2),
+('Sales Rep', 70000, 3),
+('Developer', 150000, 4)
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Mark', 'Kringle', 1, NULL)
+('John', 'Jacobs', 2, NULL)
+('Sherry', 'Cho', 3, NULL)
+('Dade', 'Murphy', 4, NULL)
