93d8e3cdd05219006ad2f44b814818d166aecd3a
Brandon Kilgore <brandonk1023@gmail.com>
2023-05-11 11:36:09 -0400

    added code into seeds database

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -4,6 +4,7 @@ VALUES
 ('Engineering'),
 ('Marketing'),
 ('IT')
+;
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
@@ -11,6 +12,8 @@ VALUES
 ('Engineer', 100000, 2),
 ('Sales Rep', 70000, 3),
 ('Developer', 150000, 4)
+;
+SELECT * FROM roles;
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
@@ -18,3 +21,10 @@ VALUES
 ('John', 'Jacobs', 2, NULL)
 ('Sherry', 'Cho', 3, NULL)
 ('Dade', 'Murphy', 4, NULL)
+('Kate', 'Libey', 4, 1)
+('Ceasar', 'Milano', 3, 2)
+('Conrad', 'Wilkes', 2, 3)
+('Michael', 'Roads', 1, 4)
+;
+
+SELECT * FROM employees;
